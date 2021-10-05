0d6d9ddf8639cf5f7133dd0cababbc2957e4876c
renu100 <renu.chandraker08@gmail.com>
2021-10-05 15:42:02 +0530

    Welcome

---
+++ main/java/com/LinkedLists/MyLinkedList.java
@@ -0,0 +1,8 @@
+package com.LinkedLists;
+
+public class MyLinkedList {
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+	}
+
+}
---
+++ test/java/com/LinkedLists/StackTest.java
@@ -0,0 +1,5 @@
+package com.LinkedLists;
+
+public class StackTest {
+
+}
